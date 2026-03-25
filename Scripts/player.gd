bfb8afc46328bdf589171b00f90eea7abd92a967
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-03-24 19:05:09 -0500

    fixes player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,15 +1,10 @@
 extends CharacterBody2D
 
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-@onready var death_plane: Area2D = $"../Death Plane"
-
 @export var MAX_SPEED := 300
 @export var JUMP_VELOCITY := -350
 
-
-
 var SPEED = 0
-var ACCELERATION = 500
+var ACCELERATION = 1000
 var CLIMB_SPEED = 200.0
 var lastX = 0
 var lastY = 0
@@ -39,7 +34,7 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2(0, 0)
 
 	# Get the input direction and handle the movement/deceleration.
-	#if direction: # Adjust the threshold (0.1) as needed:
+	if direction: # Adjust the threshold (0.1) as needed:
 		#$AnimatedSprite2D.flip_h = flip(direction)
 		
 		if did_move:
