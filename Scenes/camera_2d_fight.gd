be1db94fe8d0d039716bf328cff291aa151144c4
Odin Boywid <odinboywid@gmail.com>
2026-04-07 13:57:51 -0500

    Working towards Controller + Local Multiplayer

---
+++ Scenes/camera_2d_fight.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+@onready var p1 = %Player1
+@onready var p2 = %Player2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position = (p1.position + p2.position) / 2
