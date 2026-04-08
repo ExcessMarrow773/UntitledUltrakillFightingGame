6f1e652e44857981d66e1ae4149ed44e21d6096c
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-04-08 14:50:29 -0500

    fixes viewport zooming

--- Scenes/camera_2d_fight.gd
+++ Scenes/camera_2d_fight.gd
@@ -6,14 +6,21 @@ extends Camera2D
 func _ready() -> void:
 	pass # Replace with function body.
 
+func get_camera_rect() -> Rect2:
+	var center = self.get_screen_center_position()
+	var size = get_viewport_rect().size * zoom
+	return Rect2(center - size / 2, size)
+
 func is_on_screen(point: Vector2) -> bool:
-	var screen_rect = get_viewport_rect()
+	var screen_rect = get_camera_rect()
 	return screen_rect.has_point(point)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = (p1.position + p2.position) / 2
 	if (!is_on_screen(p1.position) or !is_on_screen(p2.position)):
-		zoom += Vector2(0.1, 0.1)
-	else:
 		zoom -= Vector2(0.1, 0.1)
+	else:
+		zoom += Vector2(0.1, 0.1)
+	zoom.x = min(1, zoom.x)
+	zoom.y = min(1, zoom.y)
