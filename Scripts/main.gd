aaece147e879ac4b195018663df30acad13776df
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-04-09 14:30:21 -0500

    adds a main.gd script

---
+++ Scripts/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("test")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
