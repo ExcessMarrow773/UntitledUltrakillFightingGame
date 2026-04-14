24e3de95f37d72eb8c97437eb522043be3bf95d8
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-04-14 14:57:12 -0500

    starts adding attack anims

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,26 +13,39 @@ func _ready() -> void:
 
 var playerInput: float
 var direction := 0
+var attack := false
+var animation = ""
+var is_attacking
 
 func _input(event):
 	device = event.device
 	if device == PLAYER_ID:
 		direction = Input.get_axis("move_left", "move_right")
+		attack = Input.is_action_pressed("attack")
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
+
 func _physics_process(delta: float) -> void:
 	var did_move = (lastX != position.x) or (lastY != position.y)
-	if direction == 0:
+	if direction == 0 and !is_attacking:
+		animation = "idle"
 		$AnimatedSprite2D.play("idle")
-	else:
+	elif !is_attacking:
+		animation = "walk"
 		$AnimatedSprite2D.play("walk")
+		
+	if attack:
+		is_attacking = true
+		animation = "attack"
+		$AnimatedSprite2D.play("attack")
 	
-	if direction < 0:
+	if direction < 0 and !is_attacking:
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.offset.x = -8
-	else:
+	elif !is_attacking and (animation != "attack"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.offset.x = 0
+	
 
 	print(direction)
 	# Add the gravity.
@@ -52,3 +65,11 @@ func _physics_process(delta: float) -> void:
 	lastY = position.y
 	lastX = position.x
 	move_and_slide()
+
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animation == "attack":
+		is_attacking = false
+		
+	animation = ""
